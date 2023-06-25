771bd5a0bb92820e1fd74bf7ee86720fe9979b94
David Ramírez <rrdavid08@gmail.com>
2023-06-25 14:45:29 +0200

    Añadida funcionalidad para cambiar nombres

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const pointsHome = document.querySelector('.points-home');
 const pointsGuest = document.querySelector('.points-guest');
-const reset = document.querySelector('.reset');
+const extraFunctions = document.querySelector('.extra-functions');
 
 const home = document.querySelector('.score-home');
 const guest = document.querySelector('.score-guest');
@@ -8,6 +8,7 @@ const guest = document.querySelector('.score-guest');
 let homeScore = 0;
 let guestScore = 0;
 
+// Añade puntos al marcador 'home'
 pointsHome.addEventListener('click', (e) => {
     if (e.target && e.target.classList.contains('plus-one')) {
         homeScore += 1;
@@ -18,10 +19,13 @@ pointsHome.addEventListener('click', (e) => {
     } else if (e.target && e.target.classList.contains('plus-three')) {
         homeScore += 3;
     };
-
+    
     home.innerText = homeScore;
 });
 
+
+
+// Añade puntos al marcador 'guest'
 pointsGuest.addEventListener('click', (e) => {
     if (e.target && e.target.classList.contains('plus-one')) {
         guestScore += 1;
@@ -36,10 +40,27 @@ pointsGuest.addEventListener('click', (e) => {
     guest.innerText = guestScore;
 });
 
-reset.addEventListener('click', () => {
-    homeScore = 0;
-    guestScore = 0;
 
-    home.innerText = homeScore
-    guest.innerText = guestScore
-});
+// Funciones extra
+extraFunctions.addEventListener('click', (e) => {
+
+    // Hace un reset de los contadores y los pone a 0
+    if (e.target && e.target.classList.contains('reset')) {
+        homeScore = 0;
+        guestScore = 0;
+    
+        home.innerText = homeScore
+        guest.innerText = guestScore
+
+    // Lanza un prompt para cambiar los nombres de los jugadores
+    } else if (e.target && e.target.classList.contains('change-name')) {
+        const titleHome = document.querySelector('.title-home');
+        const titleGuest = document.querySelector('.title-guest');
+
+        const hostName = prompt('Write the host name: ');
+        const guestName = prompt('Write the guest name: ');
+
+        titleHome.innerHTML = `<h3>${hostName}</h3>`
+        titleGuest.innerHTML = `<h3>${guestName}</h3>`
+    }
+})
